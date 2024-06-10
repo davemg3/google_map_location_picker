14cf38b6f4d77568b5a6047635fe8989f3470a73
Dave <bevilledavid@gmail.com>
2024-06-10 10:47:27 +0200

    option for removing appbar or not

--- lib/src/google_map_location_picker.dart
+++ lib/src/google_map_location_picker.dart
@@ -388,7 +388,7 @@ class LocationPickerState extends State<LocationPicker> {
             title: widget.appBarTitle != null
                 ? Text(
                     widget.appBarTitle!,
-                    style: TextStyle(color: widget.appBarTitleColor ?? Colors.black),
+                    style: TextStyle(color: widget.appBarTitleColor ?? Colors.black, fontSize: 12),
                   )
                 : SearchInput(
                     (input) => searchPlace(input),
